07724c344312080415517ebc8b6a6b6f4c460634
Jacob Buchanan <jmbucha1@asu.edu>
2016-02-22 00:50:49 -0700

    This is the first version of Calculator.java for Part 2 of Assignment 3

---
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,75 @@
+/** Name:	Jacob Buchanan
+ * 	PIN:	203
+ * 	Assignment 3
+ * 	Due:	2/22/2016
+ */
+
+package cse360assign3;
+
+public class Calculator {
+
+	private int total;
+
+	/** Calculator: This is the default constructor for the Calculator class that
+	 * sets the int "total" to zero
+	 * 
+	 * @author Jacob Buchanan
+	 */
+	public Calculator() {
+		total = 0; // not needed - included for clarity
+	}
+
+	/** getTotal: This function returns an integer 0
+	 * 
+	 * @author Jacob Buchanan
+	 * @return 0
+	 */
+	public int getTotal() {
+		return 0;
+	}
+
+	/** add: Adds the value of "value" to the value of "total"
+	 * 
+	 * @author Jacob Buchanan
+	 * @param value
+	 */
+	public void add(int value) {
+
+	}
+
+	/** subtract: Subtracts the value of "value" from the value of "total"
+	 * 
+	 * @author Jacob Buchanan
+	 * @param value
+	 */
+	public void subtract(int value) {
+
+	}
+
+	/** multiply: Multiplies the value of "value" with the value of "total"
+	 * 
+	 * @author Jacob Buchanan
+	 * @param value
+	 */
+	public void multiply(int value) {
+
+	}
+
+	/** Divides the value of "total" by the value of "value"
+	 * 
+	 * @author Jacob Buchanan
+	 * @param value
+	 */
+	public void divide(int value) {
+
+	}
+
+	/** gitHistory: Returns an empty String
+	 * 
+	 * @author Jacob Buchanan
+	 * @return ""
+	 */
+	public String getHistory() {
+		return "";
+	}
+}
